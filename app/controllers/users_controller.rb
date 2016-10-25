299265f64dc0a65748dd8e13c76ccb5fa71eb615
skylineapps <boz@skylineapps.com>
2016-10-24 20:16:58 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 		user = User.new(user_params)
 		if user.save
 			session[:user_id] = user.id
-			redirect_to '/profiles/'
+			redirect_to '/profiles/:id'
 		else
 			flash[:errors] = user.errors.full_messages
 			redirect_to :back
