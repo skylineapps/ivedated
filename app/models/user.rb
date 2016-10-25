2d5c8c3b7f2a0f91be93a6def49bf6465426de4d
skylineapps <boz@skylineapps.com>
2016-10-24 17:19:50 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>", small: "75x75>" }, default_url: "/images/:style/missing.png",
                     :url => "/assets/images/profile/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public//assets/images/profile/:id/:style/:basename.:extension"
+                    :path => ":rails_root/public/assets/images/profile/:id/:style/:basename.:extension"
 
    # validates_attachment_presence :image
    validates_attachment_size :image, :less_than =>5.megabytes
