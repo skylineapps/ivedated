3331d256ec47a6850b9f2e516cc87ea0f2f854b0
skylineapps <boz@skylineapps.com>
2016-10-26 12:18:02 -0700

    Update profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,11 @@
 class ProfilesController < ApplicationController
 	before_action :require_correct_user, only: [:edit]
+
+	def index
+		user = User.find(session[:user_id])
+		
+	end	
   def edit
-  	@user = User.find(session[:user_id])
+  	user = User.find(session[:user_id])
   end
 end
